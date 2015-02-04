bace3ee3eee805fb911a8cfab49a2ad08389630c
Thomas Williams <thomas.williams@live.co.uk>
2015-02-04 21:22:06 +0000

    Initial commit with project already started.

---
+++ SudokoChecker/SudokuGrid.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SudokoChecker
+{
+    partial class SudokuGrid
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.itsSudokuGrid = new System.Windows.Forms.DataGridView();
+            this.itsColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itsColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itsColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itsColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itsColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itsColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itsColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itsColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itsColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.itsSudokuGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // itsSudokuGrid
+            // 
+            this.itsSudokuGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.itsSudokuGrid.ColumnHeadersVisible = false;
+            this.itsSudokuGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.itsColumn1,
+            this.itsColumn2,
+            this.itsColumn3,
+            this.itsColumn4,
+            this.itsColumn5,
+            this.itsColumn6,
+            this.itsColumn7,
+            this.itsColumn8,
+            this.itsColumn9});
+            this.itsSudokuGrid.Location = new System.Drawing.Point(0, 0);
+            this.itsSudokuGrid.Name = "itsSudokuGrid";
+            this.itsSudokuGrid.RowHeadersVisible = false;
+            this.itsSudokuGrid.RowHeadersWidth = 50;
+            this.itsSudokuGrid.Size = new System.Drawing.Size(447, 447);
+            this.itsSudokuGrid.TabIndex = 0;
+            // 
+            // itsColumn1
+            // 
+            this.itsColumn1.HeaderText = "Column1";
+            this.itsColumn1.MinimumWidth = 50;
+            this.itsColumn1.Name = "itsColumn1";
+            this.itsColumn1.Width = 50;
+            // 
+            // itsColumn2
+            // 
+            this.itsColumn2.HeaderText = "Column2";
+            this.itsColumn2.MinimumWidth = 50;
+            this.itsColumn2.Name = "itsColumn2";
+            this.itsColumn2.Width = 50;
+            // 
+            // itsColumn3
+            // 
+            this.itsColumn3.HeaderText = "Column3";
+            this.itsColumn3.MinimumWidth = 50;
+            this.itsColumn3.Name = "itsColumn3";
+            this.itsColumn3.Width = 50;
+            // 
+            // itsColumn4
+            // 
+            this.itsColumn4.HeaderText = "Column4";
+            this.itsColumn4.MinimumWidth = 50;
+            this.itsColumn4.Name = "itsColumn4";
+            this.itsColumn4.Width = 50;
+            // 
+            // itsColumn5
+            // 
+            this.itsColumn5.HeaderText = "Column5";
+            this.itsColumn5.MinimumWidth = 50;
+            this.itsColumn5.Name = "itsColumn5";
+            this.itsColumn5.Width = 50;
+            // 
+            // itsColumn6
+            // 
+            this.itsColumn6.HeaderText = "Column6";
+            this.itsColumn6.MinimumWidth = 50;
+            this.itsColumn6.Name = "itsColumn6";
+            this.itsColumn6.Width = 50;
+            // 
+            // itsColumn7
+            // 
+            this.itsColumn7.HeaderText = "Column7";
+            this.itsColumn7.MinimumWidth = 50;
+            this.itsColumn7.Name = "itsColumn7";
+            this.itsColumn7.Width = 50;
+            // 
+            // itsColumn8
+            // 
+            this.itsColumn8.HeaderText = "Column8";
+            this.itsColumn8.MinimumWidth = 50;
+            this.itsColumn8.Name = "itsColumn8";
+            this.itsColumn8.Width = 50;
+            // 
+            // itsColumn9
+            // 
+            this.itsColumn9.HeaderText = "Column9";
+            this.itsColumn9.MinimumWidth = 50;
+            this.itsColumn9.Name = "itsColumn9";
+            this.itsColumn9.Width = 50;
+            // 
+            // SudokuGrid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.itsSudokuGrid);
+            this.Name = "SudokuGrid";
+            this.Size = new System.Drawing.Size(450, 450);
+            ((System.ComponentModel.ISupportInitialize)(this.itsSudokuGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView itsSudokuGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itsColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itsColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itsColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itsColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itsColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itsColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itsColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itsColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itsColumn9;
+
+
+    }
+}
